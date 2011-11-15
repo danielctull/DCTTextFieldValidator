c2e146a93398f1faedded2b3b571c2da0a9db7b0
Daniel Tull <dt@danieltull.co.uk>
2011-11-15 15:49:35 +0000

    Rename demo folder

---
+++ DCTTextFieldValidatorDemo/DCTTextFieldValidatorAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DCTTextFieldValidatorAppDelegate.h
+//  DCTTextFieldValidator
+//
+//  Created by Daniel Tull on 15.11.2011.
+//  Copyright (c) 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DCTTextFieldValidatorViewController;
+
+@interface DCTTextFieldValidatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DCTTextFieldValidatorViewController *viewController;
+
+@end
