418e64afa960e85fc5cdb77857566bc4e6a86126
Daniel Tull <dt@danieltull.co.uk>
2011-11-15 15:47:22 +0000

    Initial Commit

---
+++ DCTTextFieldValidator/DCTTextFieldValidatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  DCTTextFieldValidatorViewController.h
+//  DCTTextFieldValidator
+//
+//  Created by Daniel Tull on 15.11.2011.
+//  Copyright (c) 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCTTextFieldValidatorViewController : UIViewController
+
+@end
