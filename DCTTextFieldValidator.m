ff8165fea7c24de9838f8a8383f7197bc46e6ec9
Daniel Tull <dt@danieltull.co.uk>
2011-11-15 16:51:45 +0000

    Clean up and add test methods for control handling

--- DCTTextFieldValidator.m
+++ DCTTextFieldValidator.m
@@ -17,11 +17,24 @@
 @synthesize textFields;
 @synthesize returnPressedHandler;
 @synthesize enableButtonHandler;
+@synthesize actionControl;
+
+- (void)setTextFields:(NSArray *)tfs {
+	
+	textFields = tfs;
+	
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+	
+	for (UITextField *textField in self.textFields) {
+		textField.delegate = self;
+		textField.enablesReturnKeyAutomatically = YES;
+	}
+}
 
 #pragma mark - UITextFieldDelegate
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
-		
+	
 	if ([self dctInternal_anotherTextFieldIsEmpty:textField])
 		textField.returnKeyType = UIReturnKeyNext;
 	else
@@ -32,27 +45,30 @@
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
 	
-	if (!self.enableButtonHandler) return YES;
+	if (!self.enableButtonHandler && !self.actionControl) return YES;
 	
 	NSString *s = [textField.text stringByReplacingCharactersInRange:range withString:string];
 	
-	if (textField.returnKeyType == UIReturnKeyGo && ![s isEqualToString:@""])
-		self.enableButtonHandler(YES);
-	else
-		self.enableButtonHandler(NO);
+	if (textField.returnKeyType == UIReturnKeyGo && ![s isEqualToString:@""]) {
+		if (self.enableButtonHandler) self.enableButtonHandler(YES);
+		self.actionControl.enabled = YES;
+	} else {
+		if (self.enableButtonHandler) self.enableButtonHandler(NO);
+		self.actionControl.enabled = NO;
+	}
 	
 	return YES;
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
 	
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+	
 	if (textField.returnKeyType == UIReturnKeyGo) {
 		if (self.returnPressedHandler) self.returnPressedHandler();
 		return YES;
 	}
-	
-	__block UITextField *nextTextField = nil;
-	
+		
 	NSUInteger index = [self.textFields indexOfObject:textField];
 	
 	NSArray *end = [self.textFields subarrayWithRange:NSMakeRange(index+1, [self.textFields count] - (index+1))];
@@ -75,7 +91,7 @@
 	__block BOOL anotherTextFieldIsEmpty = NO;
 	
 	[textFields enumerateObjectsUsingBlock:^(UITextField *otherTextField, NSUInteger idx, BOOL *stop) {
-		
+				
 		if ([otherTextField isEqual:textField])
 			return;
 		
@@ -86,6 +102,49 @@
 	}];
 	
 	return anotherTextFieldIsEmpty;
+}
+
+- (IBAction)action1:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action2:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action3:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action4:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action5:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action6:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action7:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action8:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action9:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action10:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action11:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action12:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action13:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
+}
+- (IBAction)action14:(id)sender {
+	NSLog(@"%@:%@", self, NSStringFromSelector(_cmd));
 }
 
 @end
