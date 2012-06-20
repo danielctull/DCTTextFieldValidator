179b5602fe7eead54a911363d755bd3f39d313c1
Daniel Tull <dt@danieltull.co.uk>
2012-06-20 10:16:41 +0100

    Add static library

---
+++ DCTTextFieldValidatorTests/DCTTextFieldValidatorTests.h
@@ -0,0 +1,13 @@
+//
+//  DCTTextFieldValidatorTests.h
+//  DCTTextFieldValidatorTests
+//
+//  Created by Daniel Tull on 20/06/2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DCTTextFieldValidatorTests : SenTestCase
+
+@end
